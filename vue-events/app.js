9088442e2cf53ead3cc8290f417b97b96f4471b9
alexandru miroiu <55791919+alexmru@users.noreply.github.com>
2021-03-21 13:59:16 +0200

    finalizare exercitiu 2

---
+++ vue-events/app.js
@@ -0,0 +1,29 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name:'',
+      confirmedName: ''
+    };
+  },
+  methods: {
+    confirmInput() {
+      this.confirmedName = this.name;
+    },
+    submitForm() {
+      alert('Submitted!');
+      
+    },
+    setName(e, lastName) {
+      this.name = `${e.target.value} ${lastName}`;
+    },
+    add(num) {
+      this.counter += num;
+    },
+    reduce(num) {
+      this.counter -= num;
+    }
+  },
+});
+
+app.mount('#events');
