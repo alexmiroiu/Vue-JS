7e04108d7995a64670bd4aad3f98eef02d299c43
alexmru <55791919+alexmru@users.noreply.github.com>
2021-03-26 21:06:24 +0200

    app fix

--- vue-cli-new-project/src/components/FriendContact.vue
+++ vue-cli-new-project/src/components/FriendContact.vue
@@ -2,7 +2,7 @@
     <li>
         <h2>{{friend.name}}</h2>
         <button @click="toggleDetails">{{detailsAreVisible ? 'Hide' : 'Show'}} Details</button>
-        <ul v-if="detailsArevisible">
+        <ul v-if="detailsAreVisible">
             <li><strong>Phone:</strong> {{ friend.phone }}</li>
             <li><strong>Email:</strong> {{ friend.email }}</li>
         </ul>
@@ -22,7 +22,7 @@ export default {
                 }
             };
         },
-    methods: {
+        methods: {
         toggleDetails() {
             this.detailsAreVisible = !this.detailsAreVisible;
         }
