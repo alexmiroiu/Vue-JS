75d1a28c6343bb8b16d636b76517eb4468989a31
alexandru miroiu <55791919+alexmru@users.noreply.github.com>
2021-03-30 11:00:15 +0300

    Learning resources app

---
+++ Learning-resources-app/src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,26 @@
+<template>
+     <ul>
+        <learning-resource :title="res.title" :description="res.description" :link="res.link" v-for="res in resources" :key="res.id"></learning-resource>
+    </ul>
+</template>
+
+<script>
+import LearningResource from './LearningResource.vue';
+
+export default {
+    components: {
+        LearningResource
+    },
+    props: ['resources']
+}
+</script>
+
+<style scoped>
+ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    margin: auto;
+    max-width: 40rem;
+}
+</style>
