f5729bac8fd8bd5acaf7c121a332b85399e1af44
alexandru miroiu <55791919+alexmru@users.noreply.github.com>
2021-04-02 21:58:28 +0300

    vue-routing part 2

--- vue-routing/src/main.js
+++ vue-routing/src/main.js
@@ -3,15 +3,18 @@ import { createRouter, createWebHistory } from 'vue-router';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
+import NotFound from './components/nav/NotFound.vue';
 import App from './App.vue';
 
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/teams', component: TeamsList},
+        { path: '/', redirect : '/teams'},
+        { name: 'teams', path: '/teams', component: TeamsList, children: [{ name: 'team-members', path: ':teamId', component: TeamMembers, props: true}]},
         { path: '/users', component: UsersList},
-        { path: '/teams/:teamId', component: TeamMembers}
+        
+        { path: '/:notFound(.*)', component: NotFound}
     ],
     linkActiveClass: 'active'
 });
