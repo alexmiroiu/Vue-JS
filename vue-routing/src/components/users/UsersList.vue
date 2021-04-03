0b7312cc6a9c1865bebc8a5b03928823c0b529ce
alexandru miroiu <55791919+alexmru@users.noreply.github.com>
2021-04-03 13:50:54 +0300

    routing : navigation guards / scrolling behaviour

--- vue-routing/src/components/users/UsersList.vue
+++ vue-routing/src/components/users/UsersList.vue
@@ -1,5 +1,6 @@
 <template>
 <button @click="confirmInput">Confirm</button>
+<button @click="saveChanges">Save Changes</button>
   <ul>
     <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
   </ul>
@@ -12,14 +13,37 @@ export default {
   components: {
     UserItem,
   },
+  data() {
+    return {
+      changesSaved: false
+    }
+  },
   inject: ['users'],
   methods: {
     confirmInput() {
       // do something 
 
       this.$router.push('/teams');
+    },
+    saveChanges() {
+      this.changesSaved = true;
     }
   },
+  beforeRouteEnter(to, from, next) {
+    console.log('UsersList beforeRouteEnter');
+    console.log(to, from);
+    next();
+  },
+  beforeRouteLeave (to, from, next) {
+    console.log(to, from);
+    if (this.changesSaved) {
+      next();
+    } else {
+      const userWantsToLeave = confirm('Are you sure? You have unsaved changes!');
+      next(userWantsToLeave);
+    }
+
+  }
 };
 </script>
 
